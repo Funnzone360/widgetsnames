c57316dd91edf27d05462c4804934cad1af29729
Funnzone360 <hafizusman7043@gmail.com>
2024-04-24 22:36:33 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+/*
+flutter youtube channels 
+1 just flutter by Muhammad Imran 
+2 flutter hero 
+3 tech brother,s by sir Asif taj 
+4 Eazy2code flutter channel
+*/
+
+/*
+stateless widgets (no need chnages in ui);
+statefull widgets (need changes in ui at run time );
+state managment like setstate used to managed state in app its need state full widgets
+Getx is a state managment technique in which we used state less widget also we need to  make controllers also we put that widgets within obx() to update state 
+*/
+
+/*
+Material.io website 
+material ui.com
+*/
+
+/*
+Extract widget and extract method passing data through constructor 
+Expansion tile 
+Expansion list
+Stack 
+clipper
+snakebar using getx
+routes using getx
+Dark and light theme 
+flutter calculator using math expression dependency 
+Assets images network images 
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
