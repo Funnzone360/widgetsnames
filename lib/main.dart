2863b8005b7d43f9c9c74a4d4a6a0107b604a16e
Funnzone360 <hafizusman7043@gmail.com>
2024-04-27 00:19:21 -0700

    chnages liquid swipe animation

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,9 @@ Getx is a state managment technique in which we used state less widget also we n
 /*
 Material.io website 
 material ui.com
+Dafont,s
+colour picker
+
 */
 
 /*
@@ -29,6 +32,74 @@ routes using getx
 Dark and light theme 
 flutter calculator using math expression dependency 
 Assets images network images 
+floating action button
+navigator.push()
+navigator.pop()
+Get.to(routename);
+theme:themeData();
+Appbar=ubberAppbar();
+radio button 
+textspan()
+getpages[]
+padding:edgeinsets.symmetric();
+Alignment:Alignment.bottomright;
+Align
+Dropdownbutton
+Datepicker
+Circular Avatar
+AvatarGlow();dependency
+focous node
+listview.seprater
+inputchips,filterchips,choice chips ,Rowchips,
+linearprogress and circular progress indicator 
+linear step indicator 
+routatedboc-Quarterturn
+textspan-textRich()
+Drawer(),Appbar() how to open drawer from anywhere in the app
+get:dependency
+fontawesomeflutter:Dependency
+badges:Dependency
+customeclipper:googlefonts
+piechart:
+googleNavbar:dependency
+heroicon
+Boxicons
+slider
+TindercardSwiper
+circularAvatar:Background image
+Gridview.count
+Hero
+ToggleSwitch
+ToggleButton
+ToggleBar
+likeButton Dependency
+ButtonTabbar:dependency
+DynamicTabbar:
+AppinoSwiper dependency
+Screenutils
+Switchlisttile
+Autofilldependency
+otp dependency
+Stroke Text dependency
+Solution for text visibility with different backgrounds 
+1 Add a shadow 
+2 use a semi transparent background 
+3 apply a stroke 
+4 Adjust textopacity 
+5 Backdrop filter 
+6 apply gradient and background colour should be come from apis with respect to image 
+ 
+filters in flutter 
+1 colour filter 
+2 image filter 
+3 Backdrop filter 
+4 shader Mask 
+
+Bottom Navigation Bar :BottomAppBar();
+readmore dependency
+Iconsax.dependency
+flutter liquid Swipe animation dependency
+
 
 */
 
